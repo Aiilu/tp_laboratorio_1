868a5b8aff8dd1760591f15801510e2da58e576d
Aiilu <ailentorrez26@gmail.com>
2018-09-04 18:46:41 -0300

    Calculadora DONE

---
+++ TP1/Trabajo Practico 1/biblioteca.h
@@ -0,0 +1,51 @@
+#ifndef biblioteca
+
+/** \brief obtiene datos y los calcula
+ *
+ * \param void pide un valor
+ * \return float valor del numero ingresado
+ *
+ */
+void  ingresarOpcion ( float * pNumero);
+
+/** \brief obtiene datos y los calcula
+ *
+ * \param float valores a sumar
+ * \return float resultado de la suma
+ *
+ */
+float sumar(float, float);
+
+/** \brief obtiene datos y los calcula
+ *
+ * \param float valores a restar
+ * \return float resultado de la resta
+ *
+ */
+float restar(float, float);
+
+/** \brief obtiene datos y los calcula
+ *
+ * \param float valores a multiplicar
+ * \return float resultado de la multiplicacion
+ *
+ */
+float multiplicar(float, float);
+
+/** \brief obtiene datos y los calcula
+ *
+ * \param float valores a dividir
+ * \return float resultado de la division
+ *
+ */
+float dividir(float, float);
+
+/** \brief obtiene datos y los calcula
+ *
+ * \param float valores a factorizar
+ * \return float resultado de la factorizacion
+ *
+ */
+float factorial(float, float);
+
+#endif // biblioteca
